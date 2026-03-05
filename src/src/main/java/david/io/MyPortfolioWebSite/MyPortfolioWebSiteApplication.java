c92f9f3737fb8bc4d69c4f23e85125e23d4e3511
David <davidtechbuilds@gmail.com>
2026-03-05 14:45:45 +0200

    Move all files to root directory

---
+++ src/src/main/java/david/io/MyPortfolioWebSite/MyPortfolioWebSiteApplication.java
@@ -0,0 +1,13 @@
+package david.io.MyPortfolioWebSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortfolioWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortfolioWebSiteApplication.class, args);
+	}
+
+}
